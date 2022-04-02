d6991110d2eaab06208eb007f8e9c0d0cbe22662
Kirill <klimonov.dev@gmail.com>
2022-04-02 15:37:57 +0300

    flex classes

--- static/classes.js
+++ static/classes.js
@@ -1,6 +1,21 @@
 import colors from './colors.json'
 import borderRadius from './border-radius.json'
 import display from './display.json'
+import flex from './flex.json'
+import float from './float.json'
+import overflow from './overflow.json'
+import spacing from './spacing.json'
+import typography from './typography.json'
 import elevation from './elevation.json'
 
-export default [...colors, ...borderRadius, ...display, ...elevation]
+export default [
+  ...colors,
+  ...borderRadius,
+  ...display,
+  ...elevation,
+  ...flex,
+  ...float,
+  ...overflow,
+  ...spacing,
+  ...typography
+]
